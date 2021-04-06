c75cdb00253314b06f5f015786623561abdd2a9c
gladkova.o <gladkova.o@ext-system.com>
2021-04-06 15:02:36 +0700

    common_ios files

---
+++ common-ios/common_ios.h
@@ -0,0 +1,18 @@
+//
+//  common_ios.h
+//  common-ios
+//
+//  Created by Ольга on 06.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for common_ios.
+FOUNDATION_EXPORT double common_iosVersionNumber;
+
+//! Project version string for common_ios.
+FOUNDATION_EXPORT const unsigned char common_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <common_ios/PublicHeader.h>
+
+
---
+++ common-iosTests/common_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  common_iosTests.swift
+//  common-iosTests
+//
+//  Created by Ольга on 06.04.2021.
+//
+
+import XCTest
+@testable import common_ios
+
+class common_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
